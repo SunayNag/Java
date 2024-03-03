20f566814c8e80d9dbaaf3cd65ad5a51603be335
SunayNag <sunay.nagpure@gmail.com>
2024-03-04 00:31:07 +0530

    Add grid problem in Backtracking.java

--- Backtracking.java
+++ Backtracking.java
@@ -36,7 +36,37 @@ public class Backtracking{
         
     }
     
-    
+    public static int gridSolve(int si, int sj,int ei, int ej){
+        //base case
+        if(si==ei-1&&sj==ej-1){
+            return 1;
+        }
+        else if(si>=ei||sj>=ej){
+            return 0;
+        }
+        
+        //right
+        int w1=gridSolve(si+1, sj, ei, ej);
+
+        //down
+        int w2=gridSolve(si, sj+1, ei, ej);
+        return w1+w2;
+    }
+
+    public static int fact(int i){
+        if(i<=0){
+            return 1;
+        }
+        else{
+            return i*fact(i-1);
+        }
+    }
+
+    public static int gridSolveOpti(int si,int sj,int ei, int ej){
+        int ans = (fact(ei+ej-2-si-sj))/(fact(ei-1-si)*fact(ej-1-sj));
+        return ans;
+    }
+
     public static void main(String[] args) {
         // int arr[]= new int[5];
         // arrBack(arr, 0);
@@ -44,6 +74,7 @@ public class Backtracking{
         // subset("abc", new StringBuilder(), 0);
         // permutations("abc", new StringBuilder());
 
-        
+        System.out.println(gridSolve(0, 0, 10, 2));
+        System.out.println(gridSolveOpti(0, 0, 10, 0));
     }
 }
