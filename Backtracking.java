fb6ee9928f24beb95ba6bac9485110d877f24fc2
SunayNag <sunay.nagpure@gmail.com>
2024-03-05 00:37:27 +0530

    Sudoku Backtracking Problem

--- Backtracking.java
+++ Backtracking.java
@@ -67,6 +67,68 @@ public class Backtracking{
         return ans;
     }
 
+    public static boolean sudokuSolver(int[][] sudoku,int row,int col){
+        //base
+        if(row==9 ){
+            return true;
+        }
+        //recursion
+        int nextRow=row, nextCol=col+1;
+        if(col+1==9){
+            nextRow=row+1;
+            nextCol=0;
+        }
+
+        if(sudoku[row][col]!=0){
+            return sudokuSolver(sudoku, nextRow, nextCol);
+        }
+
+        for(int digit=1;digit<=9;digit++){
+            if(isSafe(sudoku, row, col, digit)){
+                sudoku[row][col] = digit;
+                if(sudokuSolver(sudoku, nextRow, nextCol)){
+                    return true;
+                }
+                sudoku[row][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSafe(int[][] sudoku, int row, int col, int digit){
+        for (int i = 0; i < 9; i++) {
+            if(sudoku[i][col]==digit){
+                return false;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if(sudoku[row][i]==digit){
+                return false;
+            }
+        }
+
+        //grid
+        int startRow= row-row%3, startCol= col-col%3;
+        for(int i=startRow;i<startRow+3;i++){
+            for(int j=startCol;j<startCol+3;j++){
+                if(sudoku[i][j]==digit){
+                    return false;
+                }
+            } 
+        }
+
+        return true;
+    }
+
+    public static void printSudoku(int[][] sudoku){
+        for(int i=0;i<sudoku.length;i++){
+            for(int j=0;j<sudoku.length;j++){
+                System.out.print(sudoku[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // int arr[]= new int[5];
         // arrBack(arr, 0);
@@ -74,7 +136,23 @@ public class Backtracking{
         // subset("abc", new StringBuilder(), 0);
         // permutations("abc", new StringBuilder());
 
-        System.out.println(gridSolve(0, 0, 10, 2));
-        System.out.println(gridSolveOpti(0, 0, 10, 0));
+        // System.out.println(gridSolve(0, 0, 10, 2));
+        // System.out.println(gridSolveOpti(0, 0, 10, 0));
+
+        int sudoku[][]= {
+        {0,0,8,0,0,0,0,0,0},
+        {4,9,0,1,5,7,0,0,2},
+        {0,0,3,0,0,4,1,9,0},
+        {1,8,5,0,6,0,0,2,0},
+        {0,0,0,0,2,0,0,6,0},
+        {9,6,0,4,0,5,3,0,0},
+        {0,3,0,0,7,2,0,0,4},
+        {0,4,9,0,3,0,0,5,7},
+        {8,2,7,0,0,9,0,1,3}
+        };
+
+        if(sudokuSolver(sudoku, 0, 0)){
+            printSudoku(sudoku);
+        }
     }
 }
